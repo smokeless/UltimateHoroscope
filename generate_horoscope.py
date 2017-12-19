715374d6e6c7a084aa049cf7e4516bbbff575730
Ross Russell <ross.russell.ca@gmail.com>
2017-12-19 14:07:07 -0800

    getting better

---
+++ generate_horoscope.py
@@ -0,0 +1,34 @@
+import markovify
+
+def selectFile(zodiacSign: str)->str:
+    '''Figure out what we're going to use as our input'''
+    txt = ''
+    with open(zodiacSign, 'r') as myFile:
+        txt = myFile.read()
+    return txt
+
+def generateScope(seed: str)->str:
+    text_model = markovify.Text(seed)
+    lines = ''
+    for i in range(1): #how many lines we generate for this horoscope.
+        lines += text_model.make_short_sentence(140, tries=25)
+        lines += '\n'
+    lines = lines.split('.')
+    for i in range(0, (len(lines)-1)):
+        if 'Audio' in lines[i] or 'AUDIO' in lines[i]:
+            del lines[i]
+    lines = '.'.join(lines)
+    return lines
+
+
+def generateAllScope():
+    signs = ['Aquarius', 'Aries', 'Cancer', 'Capricorn', 'Gemini',
+             'Leo', 'Libra', 'Pisces', 'Sagittarius', 'Scorpio',
+             'Taurus', 'Virgo']
+    for e in signs:
+        txt        = selectFile(e)
+        horoscope  = generateScope(txt)
+        print(e, 'your horoscope is:')
+        print(horoscope, '\n')
+
+generateAllScope()
